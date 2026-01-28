9141c9c48357afb4728891a6e95425a30fe9af6a
あやちねね <yanzhuchen192@gmail.com>
2026-01-28 14:33:46 +0800

    Add files via upload

---
+++ lab00/lab00.py
@@ -0,0 +1,11 @@
+def twenty_twenty_six():
+    """Come up with the most creative expression that evaluates to 2026
+    using only numbers and the +, *, and - operators (or ** and % if you'd like).
+
+    >>> twenty_twenty_six()
+    2026
+    """
+    x = 2
+    y = x*1000 + x*10 + x*3
+    return y
+
