7497017c55cf9906e6fec70fbfef63cd0ff5c0cd
あやちねね <yanzhuchen192@gmail.com>
2026-03-07 15:15:22 +0800

    Add files via upload

---
+++ hw07/hw07.scm
@@ -0,0 +1,18 @@
+(define (square n) (* n n))
+
+(define (pow base exp)
+  (if (zero? exp)
+      1
+      (* base (pow base (- exp 1 )))))
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let (y (repeatedly-cube (- n 1) x))
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (cdr s))
+
+(define (caddr s) (cddr s))
